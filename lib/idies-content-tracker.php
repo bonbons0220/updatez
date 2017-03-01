d5ece76f991953dd5c452e119c11bd12b41af1fb
bonbons0220 <bsouter@jhu.edu>
2017-03-01 14:27:35 -0500

    Added All Pages view for Status Updates. Added Status Updates Settings Page.

--- lib/idies-content-tracker.php
+++ lib/idies-content-tracker.php
@@ -120,26 +120,25 @@
 		    define( 'WP_ENV' , 'production' );
 		}
 			
-		// Add content filter
+		// filters and actions
 		add_filter( 'the_content', array($this , 'showstatus' ) );
-
-		/** Create Top Level Admin Menu item */
 		add_action( 'admin_menu', array( $this , 'create_admin_menu' ) );
-
-		// Add metabox
 		add_action( 'add_meta_boxes_page', array( $this , 'idies_update_meta_box_page' ) );		
-		
-		// Save Status Update Post Data
 		add_action('save_post', array( $this , 'idies_update_save_meta' ) );
-
 		add_action( 'manage_pages_custom_column', array( $this , 'idies_page_column_content' ) , 10 , 2 );
 		add_filter( 'manage_pages_columns', array( $this , 'idies_custom_pages_columns' ) );
+		add_filter( 'manage_edit-page_sortable_columns', array( $this , 'idies_sortable_pages_column' ) );
+		add_action( 'quick_edit_custom_box', array( $this , 'idies_display_quickedit_custom') , 10, 2 );
+		add_action( 'save_post', array(  $this , 'idies_save_quickedit_custom' ) );
+		add_action( 'admin_enqueue_scripts', array(  $this , 'idies_admin_enqueue_scripts' ) );
 		
+		
+		// ? add_action( 'manage_pages_custom_column' , array( 'custom_book_column' ), 10, 2 );
+
 		// Set up Variables
 		$this->default_reviewer = 'bsouter';
 		$this->default_status = 'not-started';
 		$this->default_tmppath = '/data1/dswww-ln01/sdss.org/tmp/';
-		//$this->default_tmppath = '/var/www/idies.jhu.edu/tmp/';
 		$this->post_status  = 'publish,private,draft';
 		
 		$this->all_users = get_users( 'orderby=nicename' );	
@@ -173,123 +172,6 @@
 			'do-not-publish' => 'panel-default',
 		);
 	}
-
-	/**
-	/* Save Meta box Data as Post Meta
-	 *
-	 * @since  1.1
-	 * @access public
-	 * @return void
-	 */	
-	function idies_update_save_meta($post_id)
-	{	
-		$newreviewer = isset( $_POST['meta_box_reviewer'] ) ? sanitize_user($_POST['meta_box_reviewer'] ) : false ;
-		$newstatus = isset( $_POST['meta_box_status'] ) ? sanitize_title($_POST['meta_box_status'])  : false ;
-		$newcomment = isset( $_POST['meta_box_comment'] ) ? sanitize_text_field($_POST['meta_box_comment']) : false ;
-		
-		if ( !( $newreviewer || $newstatus || $newcomment ) ) {
-			return $post_id;
-		}
-		
-		// Check nonce, user capabilities
-		check_admin_referer( 'update_save_meta', 'save_meta' );
-		if ( ! current_user_can( 'edit_post' ) && ! wp_is_post_autosave( $post_id ) ) return $post_id;
-
-		if ( $newreviewer && get_user_by( 'slug', $newreviewer ) ) {
-		
-			//update the post meta
-			update_post_meta(
-				$post_id,
-				'idies_update_reviewer',
-				$newreviewer
-			);
-		}
-		
-		if ( $newstatus && array_key_exists( $newstatus , $this->statuses ) ) {
-		
-			//update the post meta
-			update_post_meta(
-				$post_id,
-				'idies_update_status',
-				$newstatus
-			);
-		}
-
-		if ( $newcomment ) {
-		
-			//update the post meta
-			update_post_meta(
-				$post_id,
-				'idies_update_comment',
-				$newcomment
-			);
-		}
-	}
-
-	/**
-	/* Add Meta box to content editor
-	 *
-	 * @since  1.1
-	 * @access public
-	 * @return void
-	 */	
-	function idies_update_meta_box_page( $page ){
-		add_meta_box( 
-			'idies-update-meta-box-page', 
-			__( 'Status Update' ), 
-			array( $this , 'render_update_meta_box' ), 
-			'page', 
-			'normal', 
-			'default'
-		);
-	}
-
-	/**
-	/* Render the Status Update Meta Box in the content editor
-	 *
-	 * @since  1.1
-	 * @access public
-	 * @return void
-	 */	
-	function render_update_meta_box( $page ) {
-		$values = get_post_custom( $page->ID );
-		
-		$reviewer = isset( $values['idies_update_reviewer'] ) ? esc_attr( $values['idies_update_reviewer'][0] ) : $this->default_reviewer ;
-		$status = isset( $values['idies_update_status'] ) ? esc_attr( $values['idies_update_status'][0] ) : $this->default_status ;
-		$comment = isset( $values['idies_update_comment'] ) ? esc_attr( $values['idies_update_comment'][0] ) : '' ;
-		wp_nonce_field( 'update_save_meta', 'save_meta' );
-	?>
-	<table class="form-table meta-box">
-	<tr>
-	<th scope="row"><label for="meta_box_status">Status</label></th>
-    <td><?php
-		echo '<select  class="postbox" name="meta_box_status" id="meta-box-status">';
-		foreach ( $this->statuses as $thiskey=>$thisstatus ) {
-			echo '<option value="' . $thiskey . '"' . selected( $status , $thiskey ) . '>' . $thisstatus . '</option>';
-		}
-		echo '</select>';
-	?></td>
-	</tr>
-	<tr>
-	<th scope="row"><label for="meta_box_reviewer">Reviewer</label></th>
-    <td><?php
-		echo '<select  class="postbox" name="meta_box_reviewer" id="meta-box-reviewer">';
-		foreach ( $this->all_users as $thisuser ) {
-			echo '<option value="' . $thisuser->user_nicename . '"' . selected( $reviewer , $thisuser->user_nicename ) . '>' . $thisuser->display_name . '</option>';
-		}
-		echo '</select>';
-	?></td>
-	</tr>
-	<tr>
-	<th scope="row"><label for="meta_box_comment">Comments</label></th>
-    <td><?php
-		echo '<textarea class="postbox" rows="10" cols="60" name="meta_box_comment" id="meta-box-comment">' . $comment . '</textarea>';
-	?></td>
-	</tr>
-	</table>
-    <?php
-	}
-
 	/**
 	/* Create the Status Update Dashboard Menus
 	/* 
@@ -300,13 +182,7 @@
 	function create_admin_menu() {
 		
 		// Create the top level admin menu item
-		add_menu_page( 'Status Updates' , 'Status Updates', 'edit_pages', 'idies-status-menu' , array( $this , 'show_settings_page' ) ); 
-		
-		// Add Settings page to the submenu
-		//add_submenu_page( 'idies-status-menu' , 'Status Update Settings' , 'Settings' , 'edit_pages' , 'idies-status-settings' , array( $this , 'show_settings_page' ) );
-		
-		// Add Overview page to the submenu
-		//add_submenu_page( 'idies-status-menu' , 'Page Status Overview' , 'Overview' , 'edit_pages' , 'idies-status-overview' , array( $this , 'show_overview_page' ) );
+		add_menu_page( 'Status Updates' , 'Status Updates', 'edit_posts', 'idies-status-menu' , array( $this , 'show_settings_page' ) ); 
 		
 	}
 
@@ -321,22 +197,26 @@
 		
 		$result = array();
 		
-		if ( !current_user_can( 'edit_pages' ) )  {
+		if ( !current_user_can( 'edit_posts' ) )  {
 			wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 		}
 
 		// PAGES
 		$this->all_pages = $this->get_page_updates();
 		
+		$this->default_tmppath = get_option( 'idies_default_tmppath' , $this->default_tmppath );
+		$this->default_reviewer = get_option( 'idies_default_reviewer' , $this->default_reviewer );
+		$this->default_status = get_option( 'idies_default_status' , $this->default_status );
+
 		// Get Action
 		$the_action = ( isset( $_POST['update'] ) ? 'update' :
 					  ( isset( $_POST['nuclear'] ) ? 'nuclear' :
 					  ( isset( $_POST['import'] ) ? 'import' :
 						false ) ) );
-						
-		if ( ( $the_action ) 
-				&& ( ! empty( $_POST ) 
-				&& check_admin_referer( 'update_idies_settings' , 'update_nonce' ) ) ) {
+					
+		if ( ( $the_action ) && ( ! empty( $_POST ) ) ) {
+	
+			check_admin_referer( 'save_settings_action', 'save_settings_nonce' );
 		
 			switch ($the_action) {
 				case 'import' :
@@ -351,23 +231,23 @@
 				break;
 				case 'update' :
 				
-					$this->default_tmppath = isset( $_POST[ 'default_tmppath' ] ) ? sanitize_file_name( $_POST[ 'default_tmppath' ] ) : get_option( 'idies_default_tmppath' , $this->default_tmppath );
-					update_option( 'idies_default_tmppath' , trailingslashit( $this->default_tmppath ) );
+					if ( isset( $_POST[ 'default_tmppath' ] ) ) {
+						$this->default_tmppath = trailingslashit( $_POST[ 'default_tmppath' ] );
+						update_option( 'idies_default_tmppath' , $this->default_tmppath );
+					}
 					
-					$this->default_reviewer = isset( $_POST[ 'default_reviewer' ] ) ? sanitize_user( $_POST[ 'default_reviewer' ] ) : get_option( 'idies_default_reviewer' , $this->default_reviewer );
-					update_option( 'idies_default_reviewer' , $this->default_reviewer );
+					if ( isset( $_POST[ 'default_reviewer' ] ) ) {
+						$this->default_reviewer = sanitize_user( $_POST[ 'default_reviewer' ] );
+						update_option( 'idies_default_reviewer' , $this->default_reviewer );
+					}
 					
-					$this->default_status = isset( $_POST[ 'default_status' ] ) ? sanitize_text_field( $_POST[ 'default_status' ] ) : get_option( 'idies_default_status' , $this->default_status );
-					update_option( 'idies_default_status' , $this->default_status );
+					if ( isset( $_POST[ 'default_status' ] ) ) {
+						$this->default_status = sanitize_text_field( $_POST[ 'default_status' ] );
+						update_option( 'idies_default_status' , $this->default_status );
+					}
+					
 				break;
 			}
-			
-		} else {
-		
-			$this->default_tmppath = get_option( 'idies_default_tmppath' , $this->default_tmppath );
-			$this->default_reviewer = get_option( 'idies_default_reviewer' , $this->default_reviewer );
-			$this->default_status = get_option( 'idies_default_status' , $this->default_status );
-		
 		}
 		
 		// Write the export file each time page is loaded.
@@ -397,7 +277,7 @@
 		echo '<form method="post" action="/wp-admin/admin.php?page=idies-status-menu">';
 		echo '<input type="hidden" name="options" value="settings">';
 		echo '<input type="hidden" name="update" value="update">';
-		wp_nonce_field( 'update_idies_settings' , 'update_nonce' );
+		wp_nonce_field( 'save_settings_action' , 'save_settings_nonce' );
 		
 		echo '<table class="form-table">';
 		echo '<tbody>';
@@ -439,7 +319,7 @@
 		echo '</form>';
 
 		echo '<form method="post" action="/wp-admin/admin.php?page=idies-status-menu" enctype="multipart/form-data">';
-		wp_nonce_field( 'update_idies_settings' , 'update_nonce' );
+		wp_nonce_field( 'save_settings_action' , 'save_settings_nonce' );
 		echo '<table class="form-table">';
 		echo '<tbody>';
 
@@ -453,12 +333,6 @@
 		echo '<th scope="row">Complete</th>';
 		echo '<td>' . count( $completed_pages ) . '</td>';
 		echo '<td><em>Pages that have "Completed" status</em></td>';
-		echo '</tr>';
-		
-		echo '<tr>';
-		echo '<th scope="row">No Status Pages</th>';
-		echo '<td>' . count( $updated_pages ) . '</td>';
-		echo '<td><em>Pages with blank status can be updated on individual pages.</em></td>';
 		echo '</tr>';
 		
 		echo '<tr>';
@@ -668,6 +542,126 @@
 	
 	}		
 	
+
+	/**
+	/* Save Meta box Data from Content Editor
+	 *
+	 * @since  1.1
+	 * @access public
+	 * @return void
+	 */	
+	function idies_update_save_meta($post_id){	
+		
+		// Check nonce, user capabilities
+		if ( ! isset($_POST['save_postmeta_nonce']) ) return; //in case this is a different save_post action
+		check_admin_referer( 'save_postmeta_action', 'save_postmeta_nonce' );
+
+		$newreviewer = isset( $_REQUEST['meta_box_reviewer'] ) ? sanitize_user($_REQUEST['meta_box_reviewer'] ) : false ;
+		$newstatus = isset( $_REQUEST['meta_box_status'] ) ? sanitize_title($_REQUEST['meta_box_status'])  : false ;
+		$newcomment = isset( $_REQUEST['meta_box_comment'] ) ? sanitize_text_field($_REQUEST['meta_box_comment']) : false ;
+		
+		// Anything to update?
+		if ( !( $newreviewer || $newstatus || $newcomment ) ) {
+			return $post_id;
+		}
+
+		if ( ! current_user_can( 'edit_posts' ) && ! wp_is_post_autosave( $post_id ) ) return $post_id;
+		
+		if ( $newreviewer && get_user_by( 'slug', $newreviewer ) ) {
+		
+			//update the post meta
+			update_post_meta(
+				$post_id,
+				'idies_update_reviewer',
+				$newreviewer
+			);
+		}
+		
+		if ( $newstatus && array_key_exists( $newstatus , $this->statuses ) ) {
+		
+			//update the post meta
+			update_post_meta(
+				$post_id,
+				'idies_update_status',
+				$newstatus
+			);
+		}
+
+		if ( $newcomment ) {
+		
+			//update the post meta
+			update_post_meta(
+				$post_id,
+				'idies_update_comment',
+				$newcomment
+			);
+		}
+	}
+
+	/**
+	/* Register Meta box to content editor
+	 *
+	 * @since  1.1
+	 * @access public
+	 * @return void
+	 */	
+	function idies_update_meta_box_page( $page ){
+		add_meta_box( 
+			'idies-update-meta-box-page', 
+			__( 'Status Update' ), 
+			array( $this , 'render_update_meta_box' ), 
+			'page', 
+			'normal', 
+			'default'
+		);
+	}
+
+	/**
+	/* Render the Status Update Meta Box in the content editor
+	 *
+	 * @since  1.1
+	 * @access public
+	 * @return void
+	 */	
+	function render_update_meta_box( $page ) {
+		$values = get_post_custom( $page->ID );
+		
+		$reviewer = isset( $values['idies_update_reviewer'] ) ? esc_attr( $values['idies_update_reviewer'][0] ) : $this->default_reviewer ;
+		$status = isset( $values['idies_update_status'] ) ? esc_attr( $values['idies_update_status'][0] ) : $this->default_status ;
+		$comment = isset( $values['idies_update_comment'] ) ? esc_attr( $values['idies_update_comment'][0] ) : '' ;
+		wp_nonce_field( 'save_postmeta_action', 'save_postmeta_nonce' );
+	?>
+	<table class="form-table meta-box">
+	<tr>
+	<th scope="row"><label for="meta_box_status">Status</label></th>
+    <td><?php
+		echo '<select  class="postbox" name="meta_box_status" id="meta-box-status">';
+		foreach ( $this->statuses as $thiskey=>$thisstatus ) {
+			echo '<option value="' . $thiskey . '"' . selected( $status , $thiskey ) . '>' . $thisstatus . '</option>';
+		}
+		echo '</select>';
+	?></td>
+	</tr>
+	<tr>
+	<th scope="row"><label for="meta_box_reviewer">Reviewer</label></th>
+    <td><?php
+		echo '<select  class="postbox" name="meta_box_reviewer" id="meta-box-reviewer">';
+		foreach ( $this->all_users as $thisuser ) {
+			echo '<option value="' . $thisuser->user_nicename . '"' . selected( $reviewer , $thisuser->user_nicename ) . '>' . $thisuser->display_name . '</option>';
+		}
+		echo '</select>';
+	?></td>
+	</tr>
+	<tr>
+	<th scope="row"><label for="meta_box_comment">Comments</label></th>
+    <td><?php
+		echo '<textarea class="postbox" rows="10" cols="60" name="meta_box_comment" id="meta-box-comment">' . $comment . '</textarea>';
+	?></td>
+	</tr>
+	</table>
+    <?php
+	}
+
 	/**
 	/* Show the status on a page, under the content.
 	/* 
@@ -702,27 +696,36 @@
 	}
 
 	/**
-	/* Show a custom column that can be shown the All Pages admin screen
+	/* Show a custom column on the All Pages admin screen
 	/* 
 	 * @since  1.1
 	 * @access public
 	 * @return void
 	 */	
 	function idies_page_column_content( $column_name, $post_id ) {
-		if ( $column_name == 'reviewer' ) {
+	
+		if ( $column_name == 'update_reviewer' ) {
 			$the_user = get_user_by(  'slug', get_post_meta( $post_id , "idies_update_reviewer" , true) );
-			echo $the_user->display_name;
-
+			echo $the_user->display_name . "<span class='lookup hidden'>$the_user->user_nicename</span>";
 			} 
-		if ( $column_name == 'updatestatus' ) {
-			echo $this->statuses[get_post_meta( $post_id , "idies_update_status" , true)];
-			//get_post_meta( $post_id , "idies_update_status" , true);
-			//$the_update_status = array_key_exists( $the_update_status , $this->statuses ) ?  $this->statuses['$the_update_statuses'] : '' ;
+		if ( $column_name == 'update_status' ) {
+			$the_status = get_post_meta( $post_id , "idies_update_status" , true) ;
+			$the_user = get_user_by(  'slug', get_post_meta( $post_id , "idies_update_reviewer" , true) );
+			echo $this->statuses[ $the_status ] . "<span class='lookup hidden'>$the_status</span>";
+			if ( in_array( $the_status , array( "completed" , "do-not-publish" ) ) == false ) {
+				echo " <a class='button' href='mailto:$the_user->user_email" . "?subject=SDSS Website Update Reminder" . 
+				"&body=Hello, " . $the_user->display_name . ". \n" . 
+				"You are the reviewer for \"" . get_the_title( $post_id ) . "\" (" . get_the_permalink( $post_id ) . "). " . 
+				"This page is \"" . $this->statuses[ get_post_meta( $post_id , "idies_update_status" , true) ] . "\". " . 
+				"Please complete any required updates and mark the Update Status as \"Completed\". \n" .
+				"Thanks!" .
+				"'><strong>Send Update Reminder</strong></a>";
+			}
 		}
 	}
 
 	/**
-	/* Provides column name and header text for custom colums shown on All Pages
+	/* Register column name and header text for custom column on All Pages
 	/* 
 	 * @since  1.1
 	 * @access public
@@ -732,8 +735,8 @@
 
 		/** Add a Thumbnail Column **/
 		$myCustomColumns = array(
-			'reviewer' => __( 'Reviewer' ),
-			'updatestatus' => __( 'Update Status' )
+			'update_reviewer' => __( 'Reviewer' ),
+			'update_status' => __( 'Update Status' )
 		);
 		$columns = array_merge( $columns, $myCustomColumns );
 
@@ -744,5 +747,115 @@
 
 		return $columns;
 	}
+
+	/**
+	/* Makes custom columns sortable on All Pages screen
+	/* 
+	 * @since  1.1
+	 * @access public
+	 * @return void
+	 */	
+	function idies_sortable_pages_column( $columns ) {
+		$columns['update_reviewer'] = 'reviewer';
+		$columns['update_status'] = 'update_status';
+	 
+		//To make a column 'un-sortable' remove it from the array
+		//unset($columns['date']);
+	 
+		return $columns;
+	}
+	
+	/**
+	/* Adds custom fields to Quick Edit box on All Pages screen
+	/* 
+	 * @since  1.1
+	 * @access public
+	 * @return void
+	 */	
+	function idies_display_quickedit_custom( $column_name, $post_type ) {
+		// Only print the nonce once.
+		static $printNonce = TRUE;
+		if ( $printNonce ) {
+			$printNonce = FALSE;
+			wp_nonce_field( 'save_quickedit_action', 'save_quickedit_nonce' );
+		}
+
+		?>
+		<fieldset class="inline-edit-col-right inline-edit-idies">
+		  <div class="inline-edit-col column-<?php echo $column_name; ?>">
+			<label class="inline-edit-group">
+			<?php 
+			switch ( $column_name ) {
+				case 'update_reviewer':
+					echo '<span class="title">Update Reviewer</span><select name="update_reviewer" />';
+					foreach ( $this->all_users as $thisuser ) {
+						//echo '<option value="' . $thisuser->user_nicename . '"' . selected( $update_reviewer , $thisuser->user_nicename ) . '>' . $thisuser->display_name . '</option>';
+						echo '<option value="' . $thisuser->user_nicename . '"/>' . $thisuser->display_name . '</option>';
+					}
+					echo '</select>';
+					break;
+				case 'update_status':
+					echo '<span class="title">Update Status</span><select name="update_status" />';
+					foreach ( $this->statuses as $thiskey=>$thisvalue ) {
+						//echo '<option value="' . $thiskey . '"' . selected( $update_status , $thiskey ) . '>' . $thisvalue . '</option>';
+						echo '<option value="' . $thiskey . '">' . $thisvalue . '</option>';
+					}
+					echo '</select>';
+					break;
+			}
+			?>
+			</label>
+		  </div>
+		</fieldset>
+		<?php
+	}
+
+	/**
+	/* Saves custom fields in Quick Edit box on All Pages screen
+	/* 
+	 * @since  1.1
+	 * @access public
+	 * @return void
+	 */	
+	function idies_save_quickedit_custom( $post_id ) {
+
+		if ( ! isset($_POST['save_quickedit_nonce']) ) return; //in case this is a different save_post action
+		check_admin_referer( 'save_quickedit_action', 'save_quickedit_nonce' );
+		//if ( ! wp_verify_nonce( 'save_quickedit_nonce' , 'save_quickedit_action' ) ) return;
+		
+		if ( $this->post_type !== $_POST['post_type'] ) {
+			return;
+		}
+		if ( !current_user_can( 'edit_posts', $post_id ) ) {
+			return;
+		}
+		
+		
+		if ( isset( $_REQUEST['update_reviewer'] ) ) {
+			update_post_meta( $post_id, 'idies_update_reviewer', $_REQUEST['update_reviewer'] );
+		}
+		if ( isset( $_REQUEST['update_status'] ) ) {
+			update_post_meta( $post_id, 'idies_update_status', $_REQUEST['update_status'] );
+		}
+	}
+
+	/**
+	/* Populates fields in Quick Edit box on All Pages screen
+	/* 
+	 * @since  1.1
+	 * @access public
+	 * @return void
+	 */	
+	//if ( ! function_exists('wp_my_admin_enqueue_scripts') ):
+	function idies_admin_enqueue_scripts( $hook ) {
+
+		if ( 'edit.php' === $hook &&
+			isset( $_GET['post_type'] ) && $this->post_type === $_GET['post_type'] ) {
+				wp_enqueue_script( 'idies_admin_script', ICT_DIR_URL . 'js/admin_edit.js' ,
+					false, null, true );
+		}
+	}
+	//endif;
 	
 }
+?>
